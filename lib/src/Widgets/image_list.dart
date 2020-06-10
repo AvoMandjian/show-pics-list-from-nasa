d11cbc07bed332e25354784693c93a073a137377
Avo Mandjian <avomandjian@gmail.com>
2020-06-10 18:57:12 +0300

    finished tutorial

--- lib/src/Widgets/image_list.dart
+++ lib/src/Widgets/image_list.dart
@@ -7,6 +7,38 @@ class ImageList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-        itemCount: images.length, itemBuilder: (context, int index) {});
+      itemCount: images.length,
+      itemBuilder: (context, int index) {
+        return BuildImage(images: images[index]);
+      },
+    );
+  }
+}
+
+class BuildImage extends StatelessWidget {
+  const BuildImage({
+    Key key,
+    @required this.images,
+  }) : super(key: key);
+
+  final images;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(20),
+      decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+      margin: EdgeInsets.fromLTRB(20, 20, 20, 0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.network(images.url),
+          Padding(
+            padding: const EdgeInsets.only(top: 10.0),
+            child: Text(images.title),
+          ),
+        ],
+      ),
+    );
   }
 }
